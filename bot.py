4ec024be38bb81461de2e18404704699da8c16fb
andybeardness <beardness.andy@gmail.com>
2023-03-23 16:26:59 +0400

    add more logs

--- bot.py
+++ bot.py
@@ -64,11 +64,20 @@ def on_message(wsapp, message):
     '''
     Реакция на новое событие от сервера WS
     '''
-    log(f"OnMessage! ({killmail_count})")
+    log(f"OnMessage!")
+    print(f"killmail_count = {killmail_count}")
     killmail_count += 1
+
     
     json_string = message
     json_dict = json.loads(json_string)
+
+    try:
+        killmail_id = json_dict["killmail_id"]
+        print(f"killmail = {killmail_id}")
+    except:
+        pass
+    
 
     response = messagebuilder.response(json_dict=json_dict)
     
